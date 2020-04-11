36a34e7fa6bb003d36b1a5bc63eff04f0c2be0a8
Ron Steiner <ron.steiner@domain.com.au>
2020-04-11 11:56:10 +1000

    working mysql

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/steineron/users-service
 
 go 1.14
+
+require github.com/go-sql-driver/mysql v1.5.0
--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/steineron/users-service/morestrings"
+	"log"
+)
+
+const (
+	dbDns      = "root:Ilts0nitm@tcp(:3306)"
+	tableName  = "domain_users"
+	driverName = "mysql"
 )
 
 func main() {
+
 	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
 	fmt.Println(morestrings.ReverseRunes("Hello Go"))
 	fmt.Println(morestrings.Runes("JUST THIS"))
 
+	fmt.Println(morestrings.Runes("JUST THIS"))
+	// MySql setup using https://github.com/datacharmer/dbdeployer
+	db, err := sql.Open(driverName, dbDns+"/"+tableName)
+
+	defer db.Close()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("DB Opened")
+		// log.Printf("DB name:%q", db)
+	}
+
+	_, err = db.Exec(
+		"CREATE TABLE IF NOT EXISTS " + tableName + ".hello(world varchar(50))")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Table Created!")
+	}
 }
